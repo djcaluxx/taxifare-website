ad081b63138efef6b4ca28b31bf9ec640bbac360
lucas defaut <lucas.defaut@gmail.com>
2025-11-28 15:01:51 +0100

    My first website

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def get_map_data():
 
 df = get_map_data()
 
-st.map(df, longitude = 'long', latitude = 'lat', color = 'col', size = 100)
+st.map(df, longitude = 'long', latitude = 'lat', color = 'col', size = 1000)
 
 date_time = f"{d} {t}"
 params = {
