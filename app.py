567035c3e0fe0bfd9dc41a36f5f0b552adb683d1
lucas defaut <lucas.defaut@gmail.com>
2025-11-28 11:45:50 +0100

    My first website

--- app.py
+++ app.py
@@ -7,30 +7,8 @@ import requests
 '''
 
 st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+Just fill the fields and move on bro
 ''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 d = st.date_input(
     "Date:",
@@ -67,7 +45,6 @@ params = {
     "dropoff_latitude": dropoff_lat,
     "passenger_count": passengers
     }
-
 
 '''
 3. Let's call our API using the `requests` package...
